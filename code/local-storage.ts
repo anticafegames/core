8a1498e4974501de1c154b85a50b99b0a2abc54a
Сергей Тихомиров <tixomirov95@yandex.ru>
2020-07-17 01:16:45 +0300

    поправил di local storage

--- code/local-storage.ts
+++ code/local-storage.ts
@@ -1,19 +1,5 @@
-import { localstorageKey } from '../config/app-config'
-
 export default class LocalStorage {
-
-    static setObjectToStorage: (object: any, prefix: string) => Promise<any> = async (object: any, prefix: string) => {
-        await localStorage.setItem(`${localstorageKey}-${prefix}`, JSON.stringify(object))
-        return true
-    }
-
-    static getObjectFromStorage: (prefix: string) => Promise<any> = async (prefix: string) => {
-        const object = localStorage.getItem(`${localstorageKey}-${prefix}`)
-        return await (object && JSON.parse(object))
-    }
-
-    static clearStorage: (prefix: string) => Promise<any> = async (prefix: string) => {
-        await localStorage.removeItem(`${localstorageKey}-${prefix}`)
-        return true
-    }
+    static setObjectToStorage: (object: any, prefix: string) => Promise<any> 
+    static getObjectFromStorage: (prefix: string) => Promise<any> 
+    static clearStorage: (prefix: string) => Promise<any> 
 }
