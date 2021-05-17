9a3d5a26201c83d7b7c07b18dd48fe8ef794c12a
Сергей Тихомиров <tixomirov95@yandex.ru>
2021-05-17 15:51:19 +0300

    Новый тс видит ошибка)

--- ducks/admin/entity/main-entity.ts
+++ ducks/admin/entity/main-entity.ts
@@ -42,16 +42,16 @@ export default class MainEntity extends Record(defaultParams) {
     }
 
     setSiteInfo(siteInfo: iSiteInfo) {
-        this.set('siteInfo', new SiteInfo(siteInfo))
+        return this.set('siteInfo', new SiteInfo(siteInfo))
             .set('loading', false) as this
     }
 
     clearSiteInfo() {
-        this.set('siteInfo', undefined)
+        return this.set('siteInfo', undefined)
             .set('loading', false) as this
     }
 
     changeLoading(loading: boolean) {
-        this.set('loading', loading) as this
+        return this.set('loading', loading) as this
     }
 }
